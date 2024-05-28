5841ec544d007a77e8c0485dd519669dfc488751
ryan ardiansyah <ryanardi1922@gmail.com>
2024-05-28 00:53:40 -0700

    first commit

---
+++ diagram garis.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+xpoints = [5, 10, 15]
+ypoints = [ 50, 10, 100]
+
+plt.plot (xpoints, ypoints)
+plt.title ("diagram covid")
+plt.savefig ("diagram-garis")
+plt.show ()
